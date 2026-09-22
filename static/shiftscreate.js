d4d3cd3989340b00ec3c263e658671637e9cffc9
Julissa Gonzalez <gonzalezjulissa200@gmail.com>
2026-09-22 17:49:47 -0500

    read from html with java shift info

--- static/shiftscreate.js
+++ static/shiftscreate.js
@@ -1,10 +1,32 @@
+
 const popup = document.getElementById("shiftadding_button");
+const createshift = document.getElementById("finish_shift");
 
 popup.addEventListener("click", function() 
 {
     ShiftsEdits = document.querySelector("#PopUP_shifts");
 
     ShiftsEdits.classList.toggle("hidden")
+
+});
+
+createshift.addEventListener("click", function()
+{
+    const newShift = {};
+
+    const shift= document.querySelectorAll(".shiftDate");
+    for (let i = 0; i < shift.length; i++) 
+    {
+        const field = shift[i].dataset.field
+        const time = shift[i].value
+        console.log(field)
+        console.log(time)
+
+        newShift[field] = time;
+    }
+
+    console.log(newShift);
+
 
 });
 
